920a787aabd9b780780cd7abbe165077e1d1fa72
Birkan32 <85389327+Birkan32@users.noreply.github.com>
2021-06-28 17:23:25 +0300

    kdv tutarı hesaplandı

--- src/kdv.java
+++ src/kdv.java
@@ -1,2 +1,23 @@
+import java.util.Scanner ;
+
 public class kdv {
+    public static void main(String[] args) {
+        double kdvOran1= 0.18 , kdvTutar1, kdvliTutar1,  kdvOran2= 0.08 , kdvTutar2, kdvliTutar2 ;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ücret Tutarını Giriniz ; " ) ;
+        int tutar = input.nextInt();
+        if(tutar>0 && tutar<1000) {
+
+            kdvTutar1 = tutar * kdvOran1;
+            kdvliTutar1 = tutar + kdvTutar1;
+            System.out.println("KDV Tutari :" +  kdvTutar1 );
+        }
+        else {
+            kdvTutar2 = tutar * kdvOran2;
+            kdvliTutar1 = tutar + kdvTutar2;
+            System.out.println("KDV Tutari :" +  kdvTutar2 );
+        }
+
+    }
 }
